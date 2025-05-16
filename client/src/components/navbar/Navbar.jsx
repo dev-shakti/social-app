f6323bd14d0df4557c27ae1f9fea3f0cea6181e6
shakti sumant das <shakti.20k@gmail.com>
2025-05-16 23:54:12 +0530

    add navbar, leftbar jsx and scss

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,11 +1,49 @@
-import "./navbar.scss"
+import "./navbar.scss";
+import {Link} from "react-router-dom"
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
+import GridViewOutlinedIcon from "@mui/icons-material/GridViewOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
-    </div>
-  )
-}
+    <header className="header">
+      <div className="left">
+        <Link to="/" style={{textDecoration:"none"}}>
+           <span>lamasocial</span>
+        </Link>
+      
+        <div className="icons">
+          <HomeOutlinedIcon className="icon"/>
+          <DarkModeOutlinedIcon className="icon"/>
+          <GridViewOutlinedIcon className="icon"/>
+        </div>
+        <div className="searchContainer">
+          <SearchOutlinedIcon className="icon"/>
+          <input type="text" placeholder="Search post or videos..." />
+        </div>
+      </div>
+      <div className="right">
+        <div className="icons">
+          <PersonOutlinedIcon className="icon"/>
+          <EmailOutlinedIcon className="icon"/>
+          <NotificationsOutlinedIcon className="icon"/>
+        </div>
+        <div className="user">
+          <img
+            src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600"
+            alt=""
+          />
+          <span>John Doe</span>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- client/src/components/rightbar/RightBar.jsx
+++ client/src/components/rightbar/RightBar.jsx
@@ -1,11 +1,76 @@
-import "./rightbar.scss"
+import "./rightbar.scss";
 
 const RightBar = () => {
   return (
     <div className="rightBar">
-      RightBar 
-    </div>
-  )
-}
+      <div className="container">
+        {/* item 1 */}
+        <div className="item">
+          <span>Suggestions For You</span>
 
-export default RightBar
+            {/* user1 */}
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <span>Jane Doe</span>
+            </div>
+            <div className="buttons">
+              <button>follow</button>
+              <button>dismiss</button>
+            </div>
+          </div>
+
+          {/* user2 */}
+               <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <span>Jane Doe</span>
+            </div>
+            <div className="buttons">
+              <button>follow</button>
+              <button>dismiss</button>
+            </div>
+          </div>
+        </div>
+
+        {/* item 2 */}
+
+          <div className="item">
+          <span>Latest Activities</span>
+          
+            {/* user1 */}
+          <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+                <p>Jane Doe <span>liked the post</span> </p>
+            </div>
+             <span>5 mins ago</span>
+          </div>
+
+          {/* user2 */}
+               <div className="user">
+            <div className="userInfo">
+              <img
+                src="https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                alt=""
+              />
+              <p>Jane Doe <span>liked the post</span> </p>
+            </div>
+          <span>1 min ago</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RightBar;
