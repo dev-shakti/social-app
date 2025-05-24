79e3634a418eb6b36c8b0c78c1d2f5d239d48764
shakti sumant das <shakti.20k@gmail.com>
2025-05-24 23:53:52 +0530

    cloudinary and multer setup

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,9 +1,18 @@
 import express from "express";
 import { verifyToken } from "../middlewares/verifyToken.js";
 import { updateUserprofile } from "../controllers/user.controller.js";
+import upload from "../middlewares/multer.js";
 
 const userRoute = express.Router();
 
-userRoute.put("/:userId/update", verifyToken, updateUserprofile);
+userRoute.put(
+  "/:userId/update",
+  verifyToken,
+  upload.fields([
+    { name: "profilePic", maxCount: 1 },
+    { name: "coverPic", maxCount: 1 },
+  ]),
+  updateUserprofile
+);
 
 export default userRoute;
