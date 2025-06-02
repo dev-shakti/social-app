43f89ea3e92665f52185e2984b419e93da45a046
shakti sumant das <shakti.20k@gmail.com>
2025-06-02 23:35:50 +0530

    remove console.log from app

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -25,7 +25,7 @@ async function deletePost(id) {
     if (response?.data?.success) {
       toast.success(response.data.message);
     }
-    console.log(response.data);
+
   } catch (error) {
     console.error(error.message);
     toast.error(error.response?.data?.message || error.message);
