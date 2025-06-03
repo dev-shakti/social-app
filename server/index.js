acdba60248d0b2b7680455e035aeefe39236841d
shakti sumant das <shakti.20k@gmail.com>
2025-06-03 23:48:59 +0530

    fix cor issue

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: process.env.CLIENT_URL,
-    method: ["GET", "POST", "DELETE", "PUT"],
+    methods: ["GET", "POST", "DELETE", "PUT"],
     credentials: true,
   })
 );
